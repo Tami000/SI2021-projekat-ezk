f81234f222c4321d942a5751b3833498e30e7629
DESKTOP-QMID7LV\x <tijanaapooljcic7@gmail.com>
2022-01-15 18:23:03 +0100

    SQL file and BusinessLayer added

---
+++ BusinessLayer/KartonBusiness.cs
@@ -0,0 +1,38 @@
+﻿using DataLayer.Models;
+using DataLayer.Repositories;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public class KartonBusiness
+    {
+        KartonRepository kartonRepository;
+
+        public KartonBusiness()
+        {
+            kartonRepository = new KartonRepository();
+        }
+
+        public List<Karton> GetKarton()
+        {
+            return kartonRepository.GetKarton();
+        }
+        public bool InsertKarton(Karton k)
+        {
+            int result = kartonRepository.InsertKarton(k);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+        public bool UpdateKarton(Karton k)
+        {
+            int result = kartonRepository.UpdateKarton(k);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
---
+++ BusinessLayer/PacijentBusiness.cs
@@ -0,0 +1,47 @@
+﻿using DataLayer.Models;
+using DataLayer.Repositories;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public class PacijentBusiness
+    {
+        PacijentRepository pacijentRepository;
+
+        public PacijentBusiness()
+        {
+            pacijentRepository = new PacijentRepository();
+        }
+
+        public List<Pacijent> GetPacijent()
+        {
+            return pacijentRepository.GetPacijent();
+        }
+        public bool InsertPacijent(Pacijent p)
+        {
+            int result = pacijentRepository.InsertPacijent(p);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+        public bool UpdatePacijent(Pacijent p)
+        {
+            int result = pacijentRepository.UpdatePacijent(p);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool DeletePacijent(int pid)
+        {
+            int result = pacijentRepository.DeletePacijent(pid);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
